5258cced3d5e96e313b8482f1824a6829e6c9527
Yamazaki mao <m.s0617r.e@gmail.com>
2022-11-12 18:17:21 +0900

    footer追加

---
+++ php/footer.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang= "ja">
+<head>
+    <link rel="stylesheet" href="../style.css">
+    <meta name= "viewport" content= "width=device-width, initial-scale= 1.0">
+    <meta http-equiv= "content-type" charset= "utf-8">
+    <title>php news</title>
+</head>
+<body>
+    <footer>
+        <p> © 2022 php news</p>
+    </footer>
+</body>
